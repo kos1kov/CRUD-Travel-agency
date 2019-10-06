0cf660a1c58c6824d18a4d27e4dfd2bae92aa9cc
Dmitriy Kosikov <dima.kosikov00@mail.ru>
2019-10-07 00:03:20 +0300

    Rewrite file serializer

--- travel_agency/src/travel/data/FileSerializer.java
+++ travel_agency/src/travel/data/FileSerializer.java
@@ -2,48 +2,42 @@ package travel.data;
 
 import java.io.*;
 
-public class FileSerializer {
+public class FileSerializer<T> {
 
-    private String filename = "order.txt";
+    public static final String filename = "order.xml";
 
-    public void SaveFile(Object serObj) {
+    public  void saveFile(T serObj) throws IOException {
         File newFile = new File(filename);
-        if(newFile.exists())
+        if (newFile.exists()) {
+
+            if(!newFile.delete()){
+                throw new IOException("Could not delete file");
+            }
+
+        }
+
+        if(!newFile.createNewFile()){
+            throw new IOException("Could not create file: " + newFile.getName());
+        }
+
             try {
-                FileOutputStream fileOut = new FileOutputStream("./" + filename);
+                FileOutputStream fileOut = new FileOutputStream(newFile);
                 ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
                 objectOut.writeObject(serObj);
-                objectOut.close();
-                System.out.println("The Object  was successfully written to a file");
-
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-            else
-        try
-        {
-            boolean created = newFile.createNewFile();
-            if(created)
-                System.out.println("File has been created");
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
     }
-    public void LoadFromFile(){
-        try {
+    public T loadFromFile() throws ClassNotFoundException, IOException {
+        File newFile = new File(filename);
+        if (!newFile.exists()) {
+            throw new ClassNotFoundException ("Could not find file: " + newFile.getName());
+        }
 
-            FileInputStream fileIn = new FileInputStream(filename);
-            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
-
+        try(FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn)){
             Object obj = objectIn.readObject();
-
-            System.out.println("The Object has been read from the file");
-            objectIn.close();
-
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            return (T) obj;
         }
     }
 
